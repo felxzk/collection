29a3e5d318a51fcc048c64ac27879c61f66295fb
felipe4000 <81454661+felipe4000@users.noreply.github.com>
2025-11-21 10:33:32 -0500

    a minor update, added images to the mix and updated the skeleton generation script to match.
    I want to start working on this more often
    ** mobile optimization at some point too.

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,10 @@
 // array of items where each item has multiple properties
 const testArray = [
-    { name: 'Vivaldi', description: `hi this is my description for Vivaldi.`, tags: [`apps`, `browsers`] },
+    { name: 'Vivaldi', imagePath: 'vivaldi.svg', description: `hi this is my description for Vivaldi.`, tags: [`apps`, `browsers`] },
     { name: 'Ventoy', description: `Ventoy is pretty cool DOWNLOAD IT.`, tags: [`apps`, `ISOs`, `tools`], featured: true },
-    { name: 'CSSHero', description: `CSSHero has a variety of tools. I've only really used the gradient one.`, tags: [`development`, `design`, `tools`]}
+    { name: 'CSSHero', description: `CSSHero has a variety of tools. I've only really used the gradient one.`, tags: [`development`, `design`, `tools`, 
+        'sites'
+    ]}
 ];  
 
 // log the array to the console
@@ -19,11 +21,12 @@ function createSkelly() {
         itemWrapper.appendChild(itemElement);
         itemElement.classList.add("item");
         itemElement.innerHTML = `
-        <h2>${element.name}</h2>
-        <p>${element.description}</p>
-        <p>Tags: ${element.tags}</p>`;
-        
-    }); 
+        <h2 class="e_name">${element.name}</h2>
+        <img class="e_img"src="${`./assets/logos/${element.imagePath}` }" alt="${element.name} logo"></img>
+        <p class="e_desc">${element.description}</p>
+        <p class="e_tags">Tags: ${element.tags}</p>`;
+    }
+ ); 
 }
 
 createSkelly();
