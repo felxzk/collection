63c13fab806b76d1bd635aee67aa2526f18d7a0e
felipe4000 <81454661+felxzk@users.noreply.github.com>
2026-03-05 12:13:13 -0500

    adding bootstrap?

--- scripts/search.js
+++ scripts/search.js
@@ -1,6 +1,20 @@
 // search functionality
 // we're gonna start by defining the obvious - search field and button to fire off search - also listening for enter key potentially? 
 
+// if the user clicks on a tag, search by the tag...
+let searchBar = document.getElementById('search');
+
+searchBar.addEventListener('input', function() {
+    performSearch();
+});
+
+document.addEventListener('click', function(e) {
+    if (e.target.closest('.e_tags') && e.target.tagName === 'SPAN') {
+        searchBar.value = e.target.textContent;
+        performSearch();
+    }
+});
+
 function performSearch() {
     const query = document.getElementById('search').value.toLowerCase().trim();
 
